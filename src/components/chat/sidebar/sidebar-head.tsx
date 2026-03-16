abdc039b0410c9ef71b9789e4f2bcfa33bdcacb5
FlorinIO <florin.iordache@jordachewd.ro>
2026-03-16 17:22:25 +0200

    feat: Enhance Logo component to support icon-only rendering and update SidebarHead for improved responsiveness

--- src/components/chat/sidebar/sidebar-head.tsx
+++ src/components/chat/sidebar/sidebar-head.tsx
@@ -11,8 +11,7 @@ export default function SidebarHead({
 }: SidebarHeadProps) {
   return (
     <div className="ChatSidebarHead flex w-full items-center gap-2 bg-lightPrimary-100 p-3 dark:bg-darkPrimary-1000">
-      <Logo className={isDesktopCollapsed ? "lg:hidden" : ""} />
-      {isDesktopCollapsed && <Logo className="hidden lg:flex" size={26} />}
+      <Logo size={40} iconOnly={isDesktopCollapsed} />
     </div>
   );
 }
